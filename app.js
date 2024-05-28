d8c61a465fae6c82a438069245584f6e33677db4
HiroFr <ichiro.vf@gmail.com>
2024-05-28 21:41:26 +0200

    update : style / app.js / html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const btnValidation = document.getElementById('btn');
   const resultText = document.getElementById('result');
+  const errorText = document.getElementById('error');
 
   btnValidation.addEventListener("click", function() {
     const kmMax = parseFloat(document.getElementById('kmMax').value);
@@ -9,8 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Fonction pour calculer le pourcentage d'autonomie restante
     function calculerPourcentageAutonomie(kilometresRestants, autonomieMaximale) {
-      if (kilometresRestants > autonomieMaximale) {
-        return "Les kilomètres restants ne peuvent pas dépasser l'autonomie maximale.";
+      if(!kilometresRestants || !autonomieMaximale) {
+        errorText.style.visibility = "visible";
+        errorText.innerHTML = "Les infos ne peut pas être vide.";
+      }else if (kilometresRestants > autonomieMaximale) {
+        errorText.style.visibility = "visible";
+        errorText.innerHTML = "Les kilomètres actuels ne peuvent pas dépasser les kilomètres maximum.";
       }
       var pourcentage = (kilometresRestants / autonomieMaximale) * 100;
       return pourcentage.toFixed(2) + "%";
--- readme.md
+++ readme.md
@@ -7,4 +7,3 @@ Une application web pour avoir une estimation du pourcentage de batterie de son 
 ![Static Badge](https://img.shields.io/badge/Version-1.0.0-2187FF)
 
 > Note : Le style et les fonctionnalités de l'application web sont en cours de développement.
-
